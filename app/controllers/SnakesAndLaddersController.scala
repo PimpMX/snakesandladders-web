5020fc06b84144c1c128b79b363a4b41870980f3
lukas <lu591goe@htwg-konstanz.de>
2024-10-20 18:52:22 +0200

    Jar mit richtiger guice-Version + Controller & Routen

--- app/controllers/SnakesAndLaddersController.scala
+++ app/controllers/SnakesAndLaddersController.scala
@@ -22,10 +22,48 @@ class SnakesAndLaddersController @Inject()(val controllerComponents: ControllerC
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  
-  
+
+  def createGame(playerCount: Int) = Action { implicit request: Request[AnyContent] =>
+    controller.createGame(playerCount)
+    Ok(controller.toString())
+  }
+
+  def saveGame() = Action { implicit request: Request[AnyContent] =>
+    controller.saveGame()
+    Ok(controller.toString())
+  }
+
+  def loadGame() = Action { implicit request: Request[AnyContent] =>
+    controller.loadGame()
+    Ok(controller.toString())
+  }
+
+  def startGame() = Action { implicit request: Request[AnyContent] =>
+    controller.startGame()
+    Ok(controller.toString())
+  }
+
+  def restartGame() = Action { implicit request: Request[AnyContent] =>
+    controller.restartGame()
+    Ok(controller.toString())
+  }
+
+  def addPlayer(name: String) = Action { implicit request: Request[AnyContent] =>
+    controller.addPlayer(name)
+    Ok(controller.toString())
+  }
+
+  def rollDice() = Action { implicit request: Request[AnyContent] =>
+    controller.rollDice()
+    Ok(controller.toString())
+  }
+
+  def undoLastStep() = Action { implicit request: Request[AnyContent] =>
+    controller.undoLastAction()
+    Ok(controller.toString())
+  }
+
   def index() = Action { implicit request: Request[AnyContent] =>
-    print(controller.createGame(10))
     Ok(views.html.index())
   }
 }
