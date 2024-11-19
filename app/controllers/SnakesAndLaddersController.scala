0ad53a5b6b84d6e247717468dc529b22a5b1932b
lukas <lu591goe@htwg-konstanz.de>
2024-11-19 18:45:02 +0100

    WebSockets komplett fertig

--- app/controllers/SnakesAndLaddersController.scala
+++ app/controllers/SnakesAndLaddersController.scala
@@ -8,9 +8,10 @@ import play.api.*
 import play.api.mvc.*
 
 import scala.math.*
-import play.api.libs.json.Json
+import play.api.libs.json.{JsObject, Json}
 import play.api.libs.streams.ActorFlow
 import snakes.SnakesAndLadders
+import snakes.controller.ControllerInterface
 import snakes.util.Event.*
 import snakes.util.{Event, Observer}
 
@@ -18,72 +19,46 @@ import scala.swing.Reactor
 
 @Singleton
 class SnakesAndLaddersController @Inject()(val controllerComponents: ControllerComponents)
-  (implicit system: ActorSystem, mat: Materializer) extends BaseController {
+                                          (implicit system: ActorSystem, mat: Materializer) extends BaseController {
 
-  val controller = SnakesAndLadders.controller;
+  val controller: ControllerInterface = SnakesAndLadders.controller;
 
-  def createGame(size: Int) = Action { implicit request: Request[AnyContent] =>
+  def createGame(size: Int): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     controller.createGame(size)
-    Ok(play.api.libs.json.Json.obj(
-      "success" -> true,
-      "gameSize" -> size,
-      "message" -> s"Game of size $size created"
-    ))
+    NoContent
   }
 
-  def startGame() = Action { implicit request: Request[AnyContent] =>
+  def startGame(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     controller.startGame()
-    Redirect(routes.SnakesAndLaddersController.gameBoard())
+    NoContent
   }
 
-  def restartGame() = Action { implicit request: Request[AnyContent] =>
+  def restartGame(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     controller.restartGame()
-    Redirect(routes.SnakesAndLaddersController.index())
+    NoContent
   }
 
-  def addPlayer(playerName: String) = Action { implicit request: Request[AnyContent] =>
+  def addPlayer(playerName: String): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     controller.addPlayer(playerName)
-    val players = controller.getCurrentGameState.getPlayers.map(_.getName).toList
-    Ok(Json.obj("players" -> players))
+    NoContent
   }
 
-  def rollDice() = Action { implicit request: Request[AnyContent] =>
+  def rollDice(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     controller.rollDice()
-
-    val players = controller.getCurrentGameState.getPlayers.toList
-    val currentPlayer = controller.getCurrentGameState.getCurrentPlayer()
-    val boardSize = sqrt(controller.getBoardSize).toInt
-    val rolledValue = currentPlayer.getLastRoll
-
-    // Render
-    val boardHtml = views.html.game(boardSize, players, currentPlayer.getName, rolledValue).toString
-
-    Ok(Json.obj(
-      "boardHtml" -> boardHtml
-    ))
+    NoContent
   }
 
-  def undoLastStep() = Action { implicit request: Request[AnyContent] =>
+  def undoLastStep(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     controller.undoLastAction()
-
-    val players = controller.getCurrentGameState.getPlayers.toList
-    val currentPlayer = controller.getCurrentGameState.getCurrentPlayer()
-    val boardSize = sqrt(controller.getBoardSize).toInt
-    val rolledValue = currentPlayer.getLastRoll
-
-    // Render
-    val boardHtml = views.html.game(boardSize, players, currentPlayer.getName, rolledValue).toString
-
-    Ok(Json.obj(
-      "boardHtml" -> boardHtml
-    ))
+    NoContent
   }
 
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def indexPage(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    val players = controller.getCurrentGameState.getPlayers.toList // Convert Queue to List
+    Ok(views.html.index(players))
   }
 
-  def gameBoard() = Action { implicit request: Request[AnyContent] =>
+  def gamePage(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     val players = controller.getCurrentGameState.getPlayers.toList // Convert Queue to List
     val currentPlayer = controller.getCurrentGameState.getCurrentPlayer()
     val boardSize = sqrt(controller.getBoardSize).toInt
@@ -91,55 +66,59 @@ class SnakesAndLaddersController @Inject()(val controllerComponents: ControllerC
     Ok(views.html.game(boardSize, players, currentPlayer.getName, rolledValue))
   }
 
-  def socket = WebSocket.accept[String, String] { request =>
+  def socket: WebSocket = WebSocket.accept[String, String] { request =>
     ActorFlow.actorRef { out =>
-      println("Connect received")
+      println("New connection was established")
       SnakesAndLaddersSocketActorFactory.create(out)
     }
   }
 
-  object SnakesAndLaddersSocketActorFactory {
-    def create(out: ActorRef) = {
+  private def getBoardJson: JsObject = {
+    val players = controller.getCurrentGameState.getPlayers.toList
+    val currentPlayer = controller.getCurrentGameState.getCurrentPlayer()
+    val boardSize = sqrt(controller.getBoardSize).toInt
+    val rolledValue = currentPlayer.getLastRoll
+    val boardHtml = views.html.game(boardSize, players, currentPlayer.getName, rolledValue).toString
+    Json.obj(
+      "type" -> "htmlContent",
+      "boardHtml" -> boardHtml
+    )
+  }
+
+  private object SnakesAndLaddersSocketActorFactory {
+    def create(out: ActorRef): Props = {
       Props(new SnakesAndLaddersSocketActor(out))
     }
   }
 
-  class SnakesAndLaddersSocketActor(out: ActorRef) extends Actor with Reactor {
+  private class SnakesAndLaddersSocketActor(out: ActorRef) extends Actor with Reactor {
+
     listenTo(controller)
 
-    def receive = {
-      case msg: String =>
-        println(s"Received message: $msg")
+    def receive: Receive = {
+      case msg: String => println(s"Received message: $msg")
     }
 
     reactions += {
-      case Create =>
-        println("Reacting to Create event")
-        out ! "Create event triggered"
-      case AddPlayer =>
-        println("Reacting to AddPlayer event")
-        out ! "AddPlayer event triggered"
-      case Roll(rollResult) =>
-        println(s"Reacting to Roll event with result: $rollResult")
-        out ! s"Roll event triggered: $rollResult"
-      case Undo =>
-        println("Reacting to Undo event")
-        out ! "Undo event triggered"
-      case Start =>
-        println("Reacting to Start event")
-        out ! "Start event triggered"
-      case Load =>
-        println("Reacting to Load event")
-        out ! "Load event triggered"
-      case Save =>
-        println("Reacting to Save event")
-        out ! "Save event triggered"
-      case Update =>
-        println("Reacting to Update event")
-        out ! "Update event triggered"
-      case Restart =>
-        println("Reacting to Restart event")
-        out ! "Restart event triggered"
+      case Create => out ! Json.obj(
+        "type" -> "redirect",
+        "location" -> "/"
+      ).toString
+      case AddPlayer => out ! Json.obj(
+        "type" -> "redirect",
+        "location" -> "/"
+      ).toString
+      case Roll(rollResult) => out ! getBoardJson.toString
+      case Undo => out ! getBoardJson.toString
+      case Update => out ! getBoardJson.toString
+      case Start => out ! Json.obj(
+        "type" -> "redirect",
+        "location" -> "/game"
+      ).toString
+      case Restart => out ! Json.obj(
+        "type" -> "redirect",
+        "location" -> "/"
+      ).toString
     }
   }
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,140 +1,143 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     connectWebSocket();
+    attachListeners();
 
     function connectWebSocket() {
-        var websocket = new WebSocket("ws://localhost:9000/websocket");
+        const websocket = new WebSocket("ws://localhost:9000/websocket");
         websocket.setTimeout
 
-        websocket.onopen = function(event) {
-            console.log("Connected to Websocket");
+        websocket.onopen = function (event) {
+            console.debug("Connected to Websocket");
         }
 
         websocket.onclose = function () {
-            console.log('Connection with Websocket Closed!');
+            console.debug('Connection with Websocket Closed!');
         };
 
         websocket.onerror = function (error) {
-            console.log('Error in Websocket Occured: ' + error);
+            console.debug('Error in Websocket Occured: ' + error);
         };
 
         websocket.onmessage = function (e) {
-            console.log(e);
+
+            const message = JSON.parse(e.data);
+            console.log("Received message");
+            console.debug(message);
+
+            if(message.type === "redirect") {
+                window.location.href = message.location;
+            } else if(message.type === "htmlContent") {
+                updateGameBoard(message.boardHtml);
+            }
         };
     }
 
-    $('#undoButton').click(function() {
+    function ajaxGetRequest(url, successCallback, errorCallback) {
         $.ajax({
-            url: "/undo",
-            method: "GET",
-            dataType: "json",
-            success: function(response) {
-                $('#board').html($(response.boardHtml).find('#board').html());
-                $('#playerList').html($(response.boardHtml).find('#playerList').html());
-                $('#rollResult').html($(response.boardHtml).find('#rollResult').html());
-
-                attachRollDiceListener();
-            },
-            error: function() {
-                alert("Error undoing the last step.");
-            }
-        });
-    });
-
-    function clearPlayerList() {
-        $('#playerList').empty();
-        $('#playerList').append('<li id="noPlayersMessage" class="list-group-item">No players added yet</li>');
-    }
-
-    $('.create-game').click(function() {
-        var dimensions = $(this).data('dimensions');
-
-        $.ajax({
-            url: '/create/' + dimensions,
+            url: url,
             method: 'GET',
-            dataType: 'json',
-            success: function(response) {
-                if (response.success) {
-                    $('#gameCreationMessage').html('<div class="alert alert-success">' + response.message + '</div>');
-                    clearPlayerList();
-                } else {
-                    $('#gameCreationMessage').html('<div class="alert alert-danger">Game creation failed!</div>');
-                }
-            },
-            error: function () {
-                alert("Error creating the game.");
+            success: successCallback,
+            error: errorCallback
+        });
+    }
+
+    // All possible requests in a dictionary => calling each one will cause the server side socket
+    // actor to send a new board html, which we will then process in our websocket.onmessage handler
+
+    const requests = {
+        create: (dimensions) => {
+            ajaxGetRequest(`/create/${dimensions}`, () => {
+            }, () => {
+                alert("Error creating the game")
+            })
+        },
+        undo: () => {
+            ajaxGetRequest(`/undo`, () => {
+            }, () => {
+                alert("Error undoing the last step")
+            })
+        },
+        start: () => {
+            ajaxGetRequest(`/start`, () => {
+            }, () => {
+                alert("Error starting game")
+            })
+        },
+        restart: () => {
+            ajaxGetRequest(`/restart`, () => {
+            }, () => {
+                alert("Error restarting game")
+            })
+        },
+        roll: () => {
+            ajaxGetRequest(`/roll`, () => {
+            }, () => {
+                alert("Error rolling")
+            })
+        },
+        addPlayer: (playerName) => {
+            ajaxGetRequest(`/add/${playerName}`, () => {
+            }, () => {
+                alert("Error adding new player")
+            })
+        }
+    }
+
+    function updateGameBoard(html) {
+        $('#board').html($(html).find('#board').html());
+        $('#playerList').html($(html).find('#playerList').html());
+        $('#rollResult').html($(html).find('#rollResult').html());
+        attachListeners();
+    }
+
+    function attachListeners() {
+
+        function clearPlayerList() {
+            $('#playerList').empty();
+            $('#playerList').append('<li id="noPlayersMessage" class="list-group-item">No players added yet</li>');
+        }
+
+        // Detach and reattach listeners
+        $('#undoButton').off('click').click(() => {
+            console.log("undoing");
+            requests.undo();
+        });
+
+        $('.create-game').off('click').click(function () {
+            const dimensions = $(this).data('dimensions');
+            requests.create(dimensions);
+        });
+
+        $('#startGameButton').off('click').click(() => {
+            requests.start();
+        });
+
+        $('#restartGameButton').off('click').click(() => {
+            requests.restart();
+        });
+
+        $('#rollDiceButton').off('click').click(() => {
+            requests.roll();
+        });
+
+        function addPlayer() {
+            const name = $('#playerName').val();
+            if (!name) {
+                alert("Please enter a player name");
+                return;
             }
-        });
-    });
+            requests.addPlayer(name);
+        }
 
-    $('#startGameButton').click(function() {
-        window.location.href = "/start";
-    });
-
-    $('#restartGameButton').click(function() {
-        window.location.href = "/restart";
-    });
-
-    function attachRollDiceListener() {
-        $('#rollDiceButton').click(function() {
-            $.ajax({
-                url: "/roll",
-                method: "GET",
-                dataType: "json",
-                success: function(response) {
-                    $('#board').html($(response.boardHtml).find('#board').html());
-                    $('#playerList').html($(response.boardHtml).find('#playerList').html());
-                    $('#rollResult').html($(response.boardHtml).find('#rollResult').html());
-
-                    attachRollDiceListener();
-                },
-                error: function() {
-                    alert("Error rolling the dice.");
-                }
-            });
-        });
-    }
-
-    attachRollDiceListener();
-
-    $('#playerName').keydown(function(event) {
-        if (event.key === "Enter") {
+        $('#playerName').off('keydown').keydown((event) => {
+            if (event.key !== "Enter")
+                return;
             addPlayer();
-        }
-    });
+        });
 
-    $('#addPlayerButton').click(function() {
-        addPlayer();
-    });
-
-    function addPlayer() {
-        var playerName = $('#playerName').val();
-        if (playerName) {
-            $.ajax({
-                url: '/add/' + playerName,
-                type: 'GET',
-                dataType: 'json',
-                success: function(data) {
-                    $('#playerName').val(''); // Clear the input field
-
-                    var playerList = "";
-                    data.players.forEach(function(player) {
-                        playerList += '<li class="list-group-item">' + player + '</li>';
-                    });
-                    $('#playerList').html(playerList);
-                },
-                error: function(error) {
-                    console.error('Error adding player:', error);
-                }
-            });
-        } else {
-            alert('Please enter a player name.');
-        }
-    }
-
-    function updateGameBoard(boardHtml, playersListHtml, rolledValue) {
-        $('.board-container').html(boardHtml)
-        $('#playerList').html(playersListHtml);
-        $('#rolledValue').text('Rolled: ' + rolledValue);
+        $('#addPlayerButton').off('click').click(() => {
+            addPlayer();
+        });
     }
 });
