6d1ecfa00b66e4fe988b93557b7e2867f6517ed2
ahmedz41 <ahmed.zangana@yahoo.com>
2025-01-16 02:01:08 +0100

    authentication uodated api key

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,14 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-// Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC5V5Lyr9MKE6PCSjwq6ADRhAzjOb65aYI",
-    authDomain: "snakes-and-ladders-web.firebaseapp.com",
-    projectId: "snakes-and-ladders-web",
-    storageBucket: "snakes-and-ladders-web.firebasestorage.app",
-    messagingSenderId: "209528630302",
-    appId: "1:209528630302:web:3e61224fe8c82d7372920f",
-    measurementId: "G-XC1BYQBWXB",
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID,
+    measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -18,7 +16,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// Google login function
 export const loginWithGoogle = async () => {
     try {
         const result = await signInWithPopup(auth, provider);
